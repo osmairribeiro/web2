4a8434ffc5e5eadb5637015b76f5c48e177b35a4
Lucas Ribeiro <38088431+Lucasrcarvalho@users.noreply.github.com>
2024-03-13 00:34:06 -0300

    tela do pedido de lavagem

--- front/lol/src/app/app.component.ts
+++ front/lol/src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { CriarPedidoComponent } from './criar-pedido/criar-pedido.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [
+    RouterOutlet, 
+    HomeComponent,
+    CriarPedidoComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- front/lol/src/app/app.routes.ts
+++ front/lol/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CriarPedidoComponent } from './criar-pedido/criar-pedido.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'pedidos/novo', component: CriarPedidoComponent },
+];
