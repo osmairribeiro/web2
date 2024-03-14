2f571cd853d80411adf111219510187eda6cde69
Flinpow <thalesgc2011@hotmail.com>
2024-03-14 13:33:44 -0300

    Definindo a margem e titulo como propriedades gerais do Projeto ++ Tela de LOGIN

--- front/lol/src/app/app.routes.ts
+++ front/lol/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CriarPedidoComponent } from './criar-pedido/criar-pedido.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'pedidos/novo', component: CriarPedidoComponent },
+    { path: 'login', component: LoginComponent }
 ];
