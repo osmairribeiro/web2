5d9f96441fd1b0b08a5ef8345b0df4549103b8d7
Lucas Ribeiro <38088431+Lucasrcarvalho@users.noreply.github.com>
2024-03-21 18:04:07 -0300

    criação de header e footer como parte do template padrão

--- front/lol/src/app/app.component.ts
+++ front/lol/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CriarPedidoComponent } from './criar-pedido/criar-pedido.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +11,9 @@ import { CriarPedidoComponent } from './criar-pedido/criar-pedido.component';
   imports: [
     RouterOutlet, 
     HomeComponent,
-    CriarPedidoComponent
+    CriarPedidoComponent,
+    HeaderComponent,
+    FooterComponent
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
