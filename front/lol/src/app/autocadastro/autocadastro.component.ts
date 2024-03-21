f27b75b3a63a77873a4e6c585d21f35cc9db8dbe
Chingulo <chingulo@MacBook-Air.local>
2024-03-21 01:13:46 -0300

    Inicio da logica para salvar o cadastro

--- front/lol/src/app/autocadastro/autocadastro.component.ts
+++ front/lol/src/app/autocadastro/autocadastro.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { CadastroService } from '../cadastro.service';
+
 
 @Component({
   selector: 'app-autocadastro',
-  standalone: true,
-  imports: [],
   templateUrl: './autocadastro.component.html',
-  styleUrl: './autocadastro.component.css'
+  styleUrls: ['./autocadastro.component.css']
 })
-export class AutocadastroComponent {
+export class RegistroComponent {
+  formData: any = {};
 
+  constructor(private cadastroService: CadastroService) {}
+
+  submitForm() {
+    this.cadastroService.salvarCadastro(this.formData);
+    this.formData = {}; // Limpar o formulário após o envio
+
+     // Aqui você pode enviar os dados do formulário para o seu backend para processamento
+    // Por exemplo, você pode usar um serviço HTTP para enviar os dados para um endpoint no seu servidor
+
+    // Após processar o cadastro no backend, você pode gerar uma senha de 4 dígitos
+    const senha = this.gerarSenha();
+
+    // Agora você pode enviar um e-mail com a senha para o usuário
+    this.enviarEmail(this.formData.email, senha);
+
+    // Limpar o formulário após o envio bem-sucedido
+    this.formData = {};
+  }
 }
+
+  gerarSenha(): string {
+    // Lógica para gerar uma senha de 4 dígitos
+    const senha = Math.floor(1000 + Math.random() * 9000).toString();
+    return senha;
+  }
+
+  enviarEmail(email: string, senha: string) {
+    // Lógica para enviar um e-mail com a senha
+    console.log(`Enviando e-mail para ${email} com a senha: ${senha}`);
+    // Aqui você deve implementar o código para enviar um e-mail
+  }
+
+
