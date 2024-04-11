ac8e58b60a83de078f8c3c942893556283a37bc0
Lucas Ribeiro <38088431+Lucasrcarvalho@users.noreply.github.com>
2024-04-11 19:10:14 -0300

    Ajuste no service de consultar-pedido e criar-pedido

--- front/lol/src/app/consultar-pedido/consultar-pedido.component.ts
+++ front/lol/src/app/consultar-pedido/consultar-pedido.component.ts
@@ -8,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrl: './consultar-pedido.component.css'
 })
 export class ConsultarPedidoComponent {
-
+    @ViewChild('editClothingForm') editClothingForm!: NgForm;
+    clothing!: Clothing;
+  
+    constructor(
+        private clothingService: ClothingService,
+        private router: Router,
+        private route: ActivatedRoute,
+    ) {}
+  
+    ngOnInit(): void {
+      const id: number = +this.route.snapshot.params['id'];
+  
+      this.clothingService.findById(id).subscribe(clothingResponseDto => {
+        if (clothingResponseDto.entity === null) {
+          throw new Error('Clothing not found. ID: ' + id);
+        }
+  
+        this.clothing = clothingResponseDto.entity!;
+      });
+    }
+  
+    public async save(): Promise<void> {
+      if (this.editClothingForm.form.valid) {
+        this.clothingService.saveClothing(this.clothing).subscribe({
+          next: (dto) => {
+            this.router.navigate([RoutesEnum.LIST_CLOTHINGS])
+          },
+          error: (err) => {
+            alert(err.error.message);
+            console.log(err.error.message);
+          },
+        });
+      }
+    }
 }
--- front/lol/src/app/criar-pedido/criar-pedido.component.ts
+++ front/lol/src/app/criar-pedido/criar-pedido.component.ts
@@ -8,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrl: './criar-pedido.component.css'
 })
 export class CriarPedidoComponent {
-    
+    @ViewChild('createClothingForm') createClothingForm!: NgForm;
+    clothing!: Clothing;
+
+    constructor(private clothingService: ClothingService, private router: Router) {}
+
+    ngOnInit(): void {
+        this.clothing = new Clothing();
+    }
+
+    public async save(): Promise<void> {
+        if (this.createClothingForm.form.valid) {
+        this.clothingService.saveClothing(this.clothing).subscribe({
+            next: (clothing) => {
+            this.router.navigate([RoutesEnum.LIST_CLOTHINGS])
+            },
+            error: (err) => {
+            alert(err.error.message);
+            console.log(err.error.message);
+            },
+        });
+        }
+    }
 }
