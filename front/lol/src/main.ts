559d3f18c7aba3df3e7f0e727ba289d3943c233a
Osmair <osmairribeiro@hotmail.com>
2024-07-21 21:28:16 -0300

    Atualização Json

--- front/lol/src/main.ts
+++ front/lol/src/main.ts
@@ -3,7 +3,7 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { ModalModule } from 'ngx-bootstrap/modal';
-import { RoupaComponent } from './app/crud/roupa/roupa.component';
+
 
  bootstrapApplication(AppComponent, appConfig) 
  .catch(err => console.error(err));
