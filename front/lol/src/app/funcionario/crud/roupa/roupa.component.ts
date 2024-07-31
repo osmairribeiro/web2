29da871a54f8b423cfdb628c6f4ee897050f0dc4
Osmair <osmairribeiro@hotmail.com>
2024-07-31 20:42:31 -0300

    tratamento de retornos crud roupas

--- front/lol/src/app/funcionario/crud/roupa/roupa.component.ts
+++ front/lol/src/app/funcionario/crud/roupa/roupa.component.ts
@@ -4,8 +4,6 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RoupaModalComponent } from '../roupaModal/roupa.modal';
 import { RoupaService } from '../../../services/roupa.service';
-//import { RoupaService } from '../../services/roupa.service';
-//import { MoedaPipe } from '../../shared/pipes';
 import { MoedaPipe } from '../../../shared/pipes';
 import { ConfirmaComponent } from '../confirmModal/confirma/confirma.component';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
@@ -26,12 +24,13 @@ interface Roupa {
   providers: [RoupaService]
 })
 export class RoupaComponent implements OnInit {
-  //roupas: any[] = [];
   roupas: Roupa[] = [];
   roupaSelecionada: Roupa = { id: 0, nome: '', preco: 0, prazo: 0 };
   editando: boolean = false;
   showModal: boolean = false;
   modalRef?: BsModalRef;
+  ordenacaoAtual: { coluna: string, ascendente: boolean} = { coluna: '', ascendente: true};
+  mensagemErro: string = '';
 
   constructor(
     private roupaService: RoupaService,
@@ -42,23 +41,49 @@ export class RoupaComponent implements OnInit {
         this.listaRoupas();
   }
 
+  // listaRoupas(): void {
+  //   this.roupaService.getRoupa().subscribe({
+  //     next: (roupas) => {
+  //       //this.roupas = roupas;
+  //       this.roupas = roupas.map(roupa => ({ ...roupa, preco: parseFloat(roupa.preco.toString()) }));
+  //       console.log('Roupas recebidas:', roupas);
+  //     },
+  //     error: (err) => console.error(err),
+  //     complete: () => console.info('Lista de roupas completa')
+  //   });
+  //}
+
+
   listaRoupas(): void {
-    this.roupaService.getRoupa().subscribe({
-      next: (roupas) => {
-        //this.roupas = roupas;
-        this.roupas = roupas.map(roupa => ({ ...roupa, preco: parseFloat(roupa.preco.toString()) }));
-        console.log('Roupas recebidas:', roupas);
+    this.roupaService.getRoupa().subscribe(
+      (roupas: Roupa[]) => {
+        this.roupas = roupas;
+        //this.ordenarRoupas();
       },
-      error: (err) => console.error(err),
-      complete: () => console.info('Lista de roupas completa')
-    });
+      (error) => {
+        this.mensagemErro = error;
+      }
+    );
   }
 
-  iniciarInclusao() {
-    console.log('Iniciar Inclusão');
+  // listaRoupas(): void {
+  //   this.roupaService.getRoupa().subscribe((roupas: Roupa[]) => {
+  //     this.roupas = roupas;
+  //   });
+  // }
+
+  // iniciarInclusao() {
+  //   console.log('Iniciar Inclusão');
+  //   this.roupaSelecionada = { id: 0, nome: '', preco: 0, prazo: 0 };
+  //   this.editando = true;
+  //   this.showModal = true;
+  // }
+
+  iniciarInclusao(): void {
+    //this.roupaSelecionada = new Roupa();
     this.roupaSelecionada = { id: 0, nome: '', preco: 0, prazo: 0 };
-    this.editando = true;
     this.showModal = true;
+    this.editando = true;
   }
 
   editarRoupa(roupa: Roupa) {
@@ -68,42 +93,102 @@ export class RoupaComponent implements OnInit {
     this.showModal = true;
   }
 
-  salvarRoupa(roupa: Roupa) {
-    console.log('Salvar Roupa', roupa);
-    roupa.preco = parseFloat(roupa.preco.toString().replace(/[^\d.-]/g, ''));
-    if (roupa.id) {
-      const index = this.roupas.findIndex(r => r.id === roupa.id);
-      this.roupas[index] = roupa;
-    } else {
-      roupa.id = this.roupas.length + 1;
-      this.roupas.push(roupa);
+  // salvarRoupa(roupa: Roupa) {
+  //   console.log('Salvar Roupa', roupa);
+  //   roupa.preco = parseFloat(roupa.preco.toString().replace(/[^\d.-]/g, ''));
+  //   if (roupa.id) {
+  //     const index = this.roupas.findIndex(r => r.id === roupa.id);
+  //     this.roupas[index] = roupa;
+  //   } else {
+  //     roupa.id = this.roupas.length + 1;
+  //     this.roupas.push(roupa);
+  //   }
+  //   this.cancelar();
+ // }
+
+    // salvarRoupa(roupa: Roupa): void {
+    //   this.roupaService.saveRoupa(roupa).subscribe(() => {
+    //   this.listaRoupas();
+    //   this.cancelar();
+    // });
+    // }
+
+    salvarRoupa(roupa: Roupa): void {
+      this.roupaService.saveRoupa(roupa).subscribe(
+        () => {
+          this.listaRoupas();
+          this.cancelar();
+        },
+        (error) => {
+          this.mensagemErro = error;
+        }
+      );
     }
-    this.cancelar();
-  }
 
-  cancelar() {
-    console.log('Cancelar');
-    this.editando = false;
+
+  // cancelar() {
+  //   console.log('Cancelar');
+  //   this.editando = false;
+  //   this.showModal = false;
+  // }
+
+  cancelar(): void {
     this.showModal = false;
+    //this.roupaSelecionada = null;
+    this.editando = false;
   }
 
 
 
-  deleteRoupa(id: number) {
-    const initialState = {
-      message: 'Tem certeza que deseja excluir?'
-    };
-    this.modalRef = this.modalService.show(ConfirmaComponent, {initialState});
+  deleteRoupa(id: number): void {
+    if (confirm('Tem certeza que deseja excluir esta roupa?')) {
+      this.roupaService.deleteRoupa(id).subscribe(() => {
+        this.listaRoupas();
+      });
+    }}
 
-    this.modalRef.content.onConfirm = () => {
-      this.roupas = this.roupas.filter(roupa => roupa.id !== id);
-      this.modalRef?.hide();
-    };
+   
+    ordenarPor(coluna: keyof Roupa): void {
+      if (this.ordenacaoAtual.coluna === coluna) {
+        this.ordenacaoAtual.ascendente = !this.ordenacaoAtual.ascendente;
+      } else {
+        this.ordenacaoAtual.coluna = coluna;
+        this.ordenacaoAtual.ascendente = true;
+      }
+      this.ordenarRoupas();
+    }
+  
+    ordenarRoupas(): void {
+      const { coluna, ascendente } = this.ordenacaoAtual;
+      this.roupas.sort((a, b) => {
+        let comparacao = 0;
+        
+        if (coluna === 'id' || coluna === 'preco' || coluna === 'prazo') {
+          comparacao = a[coluna] - b[coluna];
+        } else if (coluna === 'nome') {
+          comparacao = a[coluna].localeCompare(b[coluna]);
+        }
+        
+        return ascendente ? comparacao : -comparacao;
+      });
+    }
 
-    this.modalRef.content.onDecline = () => {
-      this.modalRef?.hide();
-    };
-  }
+
+  // deleteRoupa(id: number) {
+  //   const initialState = {
+  //     message: 'Tem certeza que deseja excluir?'
+  //   };
+  //   this.modalRef = this.modalService.show(ConfirmaComponent, {initialState});
+
+  //   this.modalRef.content.onConfirm = () => {
+  //     this.roupas = this.roupas.filter(roupa => roupa.id !== id);
+  //     this.modalRef?.hide();
+  //   };
+
+  //   this.modalRef.content.onDecline = () => {
+  //     this.modalRef?.hide();
+  //   };
+  // }
 
 
   // deleteRoupa(id: number) {
@@ -111,3 +196,8 @@ export class RoupaComponent implements OnInit {
   //   this.roupas = this.roupas.filter(r => r.id !== id);
   // }
 }
+function salvarRoupa(roupa: any, Roupa: any) {
+  throw new Error('Function not implemented.');
+}
+
+
